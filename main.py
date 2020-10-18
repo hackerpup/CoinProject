24f422e5a99d00d503a4d47814e149d4546a9ff9
hackerpup <lovepup212@gmail.com>
2020-10-18 17:03:12 -0500

    Not working yet... Still in progress

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Change Calclator")
+
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+
+moneygiven = input("Enter how much money given: ")
+citem = input("How much did the item cost?: ")
+
+#Write your code here to give exact number of quarters (qmb) and dimes (dmb) and nickels (nmb) and pennies (pnb) 
+#based on the difference between citem (how much the time cost) and moneygiven (how much you paid with)
+
+
+mg = float(moneygiven)
+ci = float(citem)
+
+qmb = mg - ci/25
+dmb = qmb/10
+nmb = dmb/5
+pmb = nmb/1
+
+
+print("You need {0} quarters, {1} dimes, {2} nickels, {3} pennies.".format(qmb, dmb, nmb, pmb))
